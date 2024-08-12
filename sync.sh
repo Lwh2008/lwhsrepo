8c1e8da414df2a61ea987c481b2db2bc99b08b54
Lwh2008 <100518201+Lwh2008@users.noreply.github.com>
2024-08-12 10:53:48 +0800

    更新 sync.sh

--- sync.sh
+++ sync.sh
@@ -1,11 +1,8 @@
-sudo apt update
-sudo apt install davfs2 -y
-mkdir -p ~/.davfs2/secrets
-echo "twentyone" > ~/.davfs2/secrets/wp.hapuren.cn
-echo "twentyone" >> ~/.davfs2/secrets/wp.hapuren.cn
-sudo echo 'wp.hapuren.cn ~/.davfs2/secrets/wp.hapuren.cn' >> /etc/davfs2/secrets
-sudo systemctl restart davfs2
-mkdir sync
-sudo mount -t davfs https://wp.hapuren.cn/dav /sync
-cd sync
-rsync -avz rsync://rsync.mirrors.ustc.edu.cn/debian/* .
+#!/usr/bin/expect
+set timeout 20
+spawn mount -t davfs https://wp.hapuren.cn/dav mirrors/
+expect "Username:"
+send "twentyone\r"
+expect "Password:"
+send "twentyone\r"
+expect eof
