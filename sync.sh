af176a12070b74a9c96bc0e530fbe9943a6e9d92
Lwh2008 <100518201+Lwh2008@users.noreply.github.com>
2024-08-12 13:39:33 +0800

    更新 sync.sh

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect
 set timeout 20
-spawn mount -t davfs https://wp.hapuren.cn/dav mirrors/
+spawn mount -t davfs https://wp.hapuren.cn/123pan/user1-1/ mirrors/
 expect "Username:"
 send "twentyone\r"
 expect "Password:"
